fd341b48fc3aedf99d4d38cef9fe40b4048afeee
robinpuentes <robinpuentess@gmail.com>
2025-05-14 15:53:54 -0500

    PROCEDURE

---
+++ DML.sql
@@ -0,0 +1,10 @@
+CALL insert_fabricante(1, "Asus");
+CALL insert_fabricante(2, "lenovo");
+CALL insert_fabricante(3, "Hewlett-Packard");
+CALL insert_fabricante(4, "Samsung");
+CALL insert_fabricante(5, "Seagate");
+CALL insert_fabricante(6, "Crucial");
+CALL insert_fabricante(7, "Gigabyte");
+CALL insert_fabricante(8, "Huawei");
+CALL insert_fabricante(9, "Xiaomi");
+SELECT * FROM fabricante
