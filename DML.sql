4fc3d2fa9963725dc83bfaea5a3bf630a6ea96c6
Santiago <kevintorres1609@gmail.com>
2025-05-15 15:43:09 -0500

    update files

--- DML.sql
+++ DML.sql
@@ -1,35 +1,38 @@
-CALL insert_fabricante(1, "Asus");
-CALL insert_fabricante(2, "lenovo");
-CALL insert_fabricante(3, "Hewlett-Packard");
-CALL insert_fabricante(4, "Samsung");
-CALL insert_fabricante(5, "Seagate");
-CALL insert_fabricante(6, "Crucial");
-CALL insert_fabricante(7, "Gigabyte");
-CALL insert_fabricante(8, "Huawei");
-CALL insert_fabricante(9, "Xiaomi");
+USE db_tarea;
+CALL Insert_fabricante("Asus");
+CALL insert_fabricante("lenovo");
+CALL insert_fabricante("Hewlett-Packard");
+CALL insert_fabricante("Samsung");
+CALL insert_fabricante("Seagate");
+CALL insert_fabricante("Crucial");
+CALL insert_fabricante("Gigabyte");
+CALL insert_fabricante("Huawei");
+CALL insert_fabricante("Xiaomi");
 SELECT * FROM fabricante;
 
 
 -- ------------- Insert_fabricante 
 
-CALL insert_productos(1,"Disco_duro_SATA3_1TB",86.99, 5);
-CALL insert_productos(2,"Memoria_RAM_DDR4_8GB",120, 6);
-CALL insert_productos(3,"Disco_SSDD_1_TB",150.99,4);
-CALL insert_productos(4,"GeForce_GTX_1050Ti",185,7);
-CALL insert_productos(5,"GeForce_GTX_1080_Xtreme",755,6);
-CALL insert_productos(6,"Monitor_24_LED_Full_HD",202,1);
-CALL insert_productos(7,"Monitor_27_LED_Full_HD",245.99,1);
-CALL insert_productos(8,"Portatil_Yoga_520",559,2);
-CALL insert_productos(9,"Portatil_Ideapd_320",444,1);
-CALL insert_productos(10,"Impresora_HP_Deskjet_3720",59.99,3);
-CALL insert_productos(11,"Impresora_HP_Laserjet_Pro_M26nw",180,3);
+CALL insert_productos("Disco_duro_SATA3_1TB",86.99, 5);
+CALL insert_productos("Memoria_RAM_DDR4_8GB",120, 6);
+CALL insert_productos("Disco_SSDD_1_TB",150.99,4);
+CALL insert_productos("GeForce_GTX_1050Ti",185,7);
+CALL insert_productos("GeForce_GTX_1080_Xtreme",755,6);
+CALL insert_productos("Monitor_24_LED_Full_HD",202,1);
+CALL insert_productos("Monitor_27_LED_Full_HD",245.99,1);
+CALL insert_productos("Portatil_Yoga_520",559,2);
+CALL insert_productos("Portatil_Ideapd_320",444,1);
+CALL insert_productos("Impresora_HP_Deskjet_3720",59.99,3);
+CALL insert_productos("Impresora_HP_Laserjet_Pro_M26nw",180,3);
 
 # Ejercicio 1
 SELECT nombre FROM productos;
 # Ejercicio 2
 SELECT nombre,precio FROM productos;
 # Ejercicio 3
-SELECT nombre,precio AS precio_dolar,(precio * 0.899) AS precio_euro FROM productos;
+SELECT nombre ,precio AS precio_dolar,(precio * 0.899) AS precio_euro FROM productos;
 # Ejercicio 4
+SELECT nombre AS nombre_de_producto ,precio AS Dolares ,(precio * 0.899) AS Euro FROM productos;
+
 
 
---
+++ produce_storage.sql
@@ -0,0 +1,37 @@
+-- Proceso insert_productos
+USE `db_tarea`;
+DROP procedure IF EXISTS `insert_productos`;
+
+DELIMITER $$
+USE `db_tarea`$$
+CREATE DEFINER=root@localhost PROCEDURE insert_productos(
+IN nombre VARCHAR (100),
+IN precio double,
+IN codigo_fabricante INT(10)
+)
+BEGIN
+INSERT INTO productos(nombre, precio,codigo_fabricante)
+VALUES (nombre, precio, codigo_fabricante);
+
+END$$
+
+DELIMITER ;
+
+
+-- Proceso insert_fabricante
+USE `db_tarea`;
+DROP procedure IF EXISTS `Insert_fabricante`;
+
+DELIMITER $$
+USE `db_tarea`$$
+CREATE DEFINER=root@localhost PROCEDURE Insert_fabricante(
+IN nombre VARCHAR (100)
+)
+BEGIN
+INSERT INTO fabricante(nombre)
+VALUES(nombre);
+
+END$$
+
+DELIMITER ;
+
